b3e418fad87eef829f55d5214b9bcdaae8edcade
Saud Iqbal <saudiqbal886@gmail.com>
2023-01-05 22:26:16 +0530

    ethereum developer bootcamp week-2 assignment

--- ethereum-developer-bootcamp/week-2/GiftList/server/index.js
+++ ethereum-developer-bootcamp/week-2/GiftList/server/index.js
@@ -1,25 +1,28 @@
-const express = require('express');
-const verifyProof = require('../utils/verifyProof');
+const express = require("express");
+
+const MerkleTree = require("../utils/MerkleTree");
+const verifyProof = require("../utils/verifyProof");
+
+const niceList = require("../utils/niceList");
+
+const merkleTree = new MerkleTree(niceList);
 
 const port = 1225;
 
 const app = express();
 app.use(express.json());
 
-// TODO: hardcode a merkle root here representing the whole nice list
-// paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = merkleTree.getRoot();
 
-app.post('/gift', (req, res) => {
-  // grab the parameters from the front-end here
+app.post("/gift", (req, res) => {
   const body = req.body;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
+  const { proof, name } = body;
+
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+  if (isInTheList) {
     res.send("You got a toy robot!");
-  }
-  else {
+  } else {
     res.send("You are not on the list :(");
   }
 });
